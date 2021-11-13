21927195ce70514bc974fd28eaf7ad8cb9edf2be
julie <julie@moveparallel.com>
2021-11-12 21:47:38 -0800

    checkpoint, make main function more pythonic, cleaning up training functions

--- main.py
+++ main.py
@@ -17,21 +17,26 @@ from models.tester import model_test
 import torch
 import argparse
 
-parser = argparse.ArgumentParser()
-# F parameter
-parser.add_argument('--n_his', type=int, default=12)
-parser.add_argument('--n_pred', type=int, default=9)
-parser.add_argument('--batch_size', type=int, default=50)
-parser.add_argument('--epoch', type=int, default=50)
-parser.add_argument('--save', type=int, default=10)
-parser.add_argument('--lr', type=float, default=1e-3)
-parser.add_argument('--opt', type=str, default='RMSProp')
-parser.add_argument('--graph', type=str, default='default')
-parser.add_argument('--inf_mode', type=str, default='merge')
-args = parser.parse_args()
-print(f'Training configs: {args}')
+def parse_args():
+    """
+    Parse input arguments
+    """
+    parser = argparse.ArgumentParser()
+    # F parameter
+    parser.add_argument('--n_his', type=int, default=12)
+    parser.add_argument('--n_pred', type=int, default=9)
+    parser.add_argument('--batch_size', type=int, default=50)
+    parser.add_argument('--epoch', type=int, default=50)
+    parser.add_argument('--save', type=int, default=10)
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--opt', type=str, default='RMSProp')
+    parser.add_argument('--graph', type=str, default='PeMSD7_W_228', help='Graph name defaults to PeMSD7_W_228')
+    parser.add_argument('--graph_data', type=str, default='PeMSD7_V_228', help='Graph dataset name defaults to PeMSD7_V_228')
+    parser.add_argument('--inf_mode', type=str, default='merge')
 
-# Config that's passed around
+    return parser.parse_args()
+
+# Constant config to use througout
 config = {
     'C_BATCH_SIZE': 50,
     'C_EPOCHS': 150,
@@ -39,21 +44,27 @@ config = {
     'C_INITIAL_LR': 2e-4
 }
 
-# Load weighted adjacency matrix W
-if args.graph == 'default':
-    W = weight_matrix(os.path.join('./dataset', f'PeMSD7_W_228.csv'))
-else:
-    # load customized graph weight matrix
-    W = weight_matrix(os.path.join('./dataset', args.graph))
+def main():
+    """
+    Main function to train and test a model.
+    """
+    args = parse_args()
 
-# Data Preprocessing and loading
-# TODO make filename configurable
-data = pd.read_csv('./dataset/PeMSD7_V_228.csv', header=None).values
-dataset = TrafficDataset(data, args.n_his, args.n_pred)
-(train, val, test) = get_splits(dataset)
-train_dataloader = torch.utils.data.DataLoader(train, batch_size=config['C_BATCH_SIZE'], shuffle=True)
-val_dataloader = torch.utils.data.DataLoader(val, batch_size=config['C_BATCH_SIZE'], shuffle=True)
-test_dataloader = torch.utils.data.DataLoader(test, batch_size=config['C_BATCH_SIZE'], shuffle=True)
+    # Load weighted adjacency matrix W
+    W = weight_matrix(os.path.join('./dataset', args.graph + '.csv'))
 
-model_train(train_dataloader, config)
-model_test(test_dataloader, config)
+    # Data Preprocessing and loading
+    data = pd.read_csv(os.path.join('./dataset', args.graph_data + '.csv'), header=None).values
+    dataset = TrafficDataset(data, args.n_his, args.n_pred)
+    (train, val, test) = get_splits(dataset, (0.6, 0.2, 0.2))
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=config['C_BATCH_SIZE'], shuffle=True)
+    val_dataloader = torch.utils.data.DataLoader(val, batch_size=config['C_BATCH_SIZE'], shuffle=True)
+    test_dataloader = torch.utils.data.DataLoader(test, batch_size=config['C_BATCH_SIZE'], shuffle=True)
+
+    # Train model
+    model_train(train_dataloader, val_dataloader, config)
+    # Test model
+    model_test(test_dataloader, config)
+
+if __name__ == "__main__":
+    main()
--- models/trainer.py
+++ models/trainer.py
@@ -10,27 +10,37 @@ from models.st_gat import ST_GAT
 import torch
 import torch.optim as optim
 import numpy as np
-import time
 import pandas as pd
+from tqdm import tqdm
 
-def train(model, data, train_idx, optimizer, loss_fn):
+def train(model, device, dataloader, optimizer, loss_fn):
     """
     Forward pass and backward pass on a model
     """
     model.train()
     loss=0
 
-    optimizer.zero_grad()
-    y_pred= model(data.x)
-    loss = loss_fn(y_pred[train_idx], data.y[train_idx])
+    for _, batch in enumerate(tqdm(dataloader, desc="Iteration")):
+        batch = batch.to(device)
 
-    loss.backward()
-    optimizer.step()
+        if batch.x.shape[0] == 1 or batch.batch[-1] == 0:
+            pass
+        else:
+            optimizer.zero_grad()
+            y_pred = torch.squeeze(model(batch))
+            loss = loss_fn(y_pred, torch.squeeze(batch.y).float())
+            loss.backward()
+            optimizer.step()
 
     return loss.item()
 
+
+
 @torch.no_grad()
-def test(model, data, split_idx, evaluator):
+def eval(model, device, dataloader, evaluator):
+    """
+    Evaluation function to evaluate model on data
+    """
     model.eval()
     out = None
     out = model(data.x)
@@ -51,36 +61,25 @@ def test(model, data, split_idx, evaluator):
 
     return train_acc, valid_acc, test_acc
 
-def model_train(train_dataset, val_dataset, config):
+def model_train(train_dataloader, val_dataloader, config):
     """
-    Train the ST-GAT model.
+    Train the ST-GAT model. Evaluate on validation dataset as you go.
     """
-    # Load and preprocess the data
+    # Get GPU if you can
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    # If you use GPU, the device should be cuda
-    print('Device: {}'.format(device))
-    data = train_dataset.to(device)
-    split_idx = train_dataset.get_idx_split()
-    train_idx = split_idx['train'].to(device)
-
-    # train_dataset shape: T x F x N
-    model = ST_GAT(in_channels=train_dataset.shape[1::], out_channels=train_dataset.shape[1::])
-    # TODO not sure what this args should be?
+    # Make the model. TODO add RNN here
+    model = ST_GAT(in_channels=train_dataloader.shape[1::], out_channels=train_dataloader.shape[1::])
     optimizer = optim.Adam(lr=config.C_INITIAL_LR, weight_decay=config.C_WEIGHT_DECAY)
     loss_fn = torch.nn.MSELoss
 
-    model.train()
+    # For every epoch, train the model on training dataset. Evaluate model on validation dataset
     for epoch in range(config.C_EPOCHS):
-        loss = train(model, data, train_idx, optimizer, loss_fn)
-        # TODO define the evaluator!!
-        train_acc, valid_acc, test_acc = test(model, data, split_idx, evaluator)
+        loss = train(model, device, train_dataloader, optimizer, loss_fn)
+        train_result = eval(model, device, train_dataloader, evaluator)
+        val_result = eval(model, device, val_dataloader, evaluator)
         # TODO add tensorboard to visualize training over time
-        print(f'Epoch: {epoch:02d}, '
-          f'Loss: {loss:.4f}, '
-          f'Train: {100 * train_acc:.2f}%, '
-          f'Valid: {100 * valid_acc:.2f}% '
-          f'Test: {100 * test_acc:.2f}%')
+        print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Train: {100 * train_acc:.2f}%, Valid: {100 * valid_acc:.2f}%')
 
 def model_test(dataset, config):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
