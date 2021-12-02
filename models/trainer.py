adb2a5a10713a402376177e19330e0e14442ec37
julie <julie@moveparallel.com>
2021-12-01 21:31:50 -0800

    dataloader working! fixing training

--- models/trainer.py
+++ models/trainer.py
@@ -66,7 +66,6 @@ def model_train(train_dataloader, val_dataloader, config):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # Make the model. TODO add RNN here
-    assert train_dataloader.dataset.shape[1::] == val_dataloader.dataset.shape[1::]
     model = ST_GAT(in_channels=train_dataloader.dataset.shape[1::], out_channels=train_dataloader.dataset.shape[1::])
     optimizer = optim.Adam(model.parameters(), lr=config['C_INITIAL_LR'], weight_decay=config['C_WEIGHT_DECAY'])
     loss_fn = torch.nn.MSELoss
