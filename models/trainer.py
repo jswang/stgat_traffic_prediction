665b3ab0fa4d593569f6484531f1e114ea99687a
julie <julie@moveparallel.com>
2021-12-04 22:20:40 -0800

    Tensorboard added

--- models/trainer.py
+++ models/trainer.py
@@ -11,22 +11,10 @@ from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, 
 
 from models.st_gat import ST_GAT
 from utils.math_utils import *
+from torch.utils.tensorboard import SummaryWriter
 
-def train(model, device, dataloader, optimizer, loss_fn):
-    """
-    Forward pass and backward pass on a model
-    """
-    model.train()
-    loss=0
-    for _, batch in enumerate(tqdm(dataloader, desc="Iteration")):
-        batch = batch.to(device)
-        optimizer.zero_grad()
-        y_pred = torch.squeeze(model(batch))
-        loss = loss_fn()(y_pred.float(), torch.squeeze(batch.y).float())
-        loss.backward()
-        optimizer.step()
-
-    return loss.item()
+# Make a tensorboard writer
+writer = SummaryWriter()
 
 @torch.no_grad()
 def eval(model, device, dataloader, type=''):
@@ -68,25 +56,44 @@ def model_train(train_dataloader, val_dataloader, config):
     """
     Train the ST-GAT model. Evaluate on validation dataset as you go.
     """
+
     print("Training Model")
     # Get GPU if you can
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    # Make the model.
-    # each datapoint in the graph is 228x12: N x F (N = # nodes, F = time window)
+    # Make the model. Each datapoint in the graph is 228x12: N x F (N = # nodes, F = time window)
     # TODO pass in n_hist and n_pred better
     model = ST_GAT(in_channels=config['n_hist'], out_channels=config['n_pred'], num_nodes=config['n_node'])
     optimizer = optim.Adam(model.parameters(), lr=config['C_INITIAL_LR'], weight_decay=config['C_WEIGHT_DECAY'])
     loss_fn = torch.nn.MSELoss
 
-    # For every epoch, train the model on training dataset. Evaluate model on validation dataset
-    for epoch in range(1):#config['C_EPOCHS']): # only do a couple of epochs for now to see what's happening
-        loss = train(model, device, train_dataloader, optimizer, loss_fn)
+    model.train()
 
-        if epoch %10 == 0:
+    # For every epoch, train the model on training dataset. Evaluate model on validation dataset
+    for epoch in range(5):#config['C_EPOCHS']): # only do a couple of epochs for now to see what's happening
+
+        for _, batch in enumerate(tqdm(train_dataloader, desc="Iteration")):
+            batch = batch.to(device)
+            optimizer.zero_grad()
+            y_pred = torch.squeeze(model(batch))
+            loss = loss_fn()(y_pred.float(), torch.squeeze(batch.y).float())
+            writer.add_scalar("Loss/train", loss, epoch)
+            loss.backward()
+            optimizer.step()
+
+        if epoch % 1 == 0:
             print(f"Epoch {epoch}, loss: {loss}")
-            eval(model, device, train_dataloader, 'Train')
-            eval(model, device, val_dataloader, 'Valid')
+            train_mae, train_rmse, train_mape = eval(model, device, train_dataloader, 'Train')
+            val_mae, val_rmse, val_mape = eval(model, device, val_dataloader, 'Valid')
+            writer.add_scalar("MAE/train", train_mae, epoch)
+            writer.add_scalar("RMSE/train", train_rmse, epoch)
+            writer.add_scalar("MAPE/train", train_mape, epoch)
+
+            writer.add_scalar("MAE/val", val_mae, epoch)
+            writer.add_scalar("RMSE/val", val_rmse, epoch)
+            writer.add_scalar("MAPE/val", val_mape, epoch)
+
+    writer.flush()
 
     return model
 
