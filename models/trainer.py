910121daf0416fc0b8379ba4731270576ebae529
amelia22974 <amelia.a.woodward@gmail.com>
2021-12-04 21:28:30 -0800

    Update eval to include rmse, mae, mape averaged over minibatches

--- models/trainer.py
+++ models/trainer.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, mean_absolute_error
 
 from models.st_gat import ST_GAT
 from utils.math_utils import *
@@ -29,7 +30,7 @@ def train(model, device, dataloader, optimizer, loss_fn):
     return loss.item()
 
 @torch.no_grad()
-def eval(model, device, dataloader):
+def eval(model, device, dataloader, num_batches=50):
     """
     Evaluation function to evaluate model on data
 
@@ -50,12 +51,22 @@ def eval(model, device, dataloader):
 
             y_true.append(batch.y.view(pred.shape).detach().cpu())
             y_pred.append(pred.detach().cpu())
-
-    # update to be accuracy metric from paper (we might want to compute multiple types of accuracy metrics here)
-    acc = 0
-
-    return acc
-    # TODO return something
+    
+    # actually need to convert this back into speeds given vectors
+    # update to be accuracy metric from paper (we might want to compute multiple types of accuracy metrics here
+    rmse = 0
+    mae = 0
+    mape = 0
+    for i in range(num_batches): # replace this with official arg for number of batches
+        # the final index in the feature gives the velocity that we want
+        vel_true = y_true[i][:, -1:]
+        vel_pred = y_pred[i][:, -1:]
+        rmse += mean_squared_error(vel_true, vel_pred, squared=False)
+        mae += mean_absolute_error(vel_true, vel_pred)
+        mape += mean_absolute_percentage_error(vel_true, vel_pred)
+    
+    #get the average score for each metric in each batch
+    return rmse / num_batches, mae / num_batches, mape / num_batches
 
 
 def model_train(train_dataloader, val_dataloader, config):
@@ -76,6 +87,7 @@ def model_train(train_dataloader, val_dataloader, config):
     # For every epoch, train the model on training dataset. Evaluate model on validation dataset
     for epoch in range(0,1):#config['C_EPOCHS']): # only do a couple of epochs for now to see what's happening
         loss = train(model, device, train_dataloader, optimizer, loss_fn)
+        print(loss)
         train_result = eval(model, device, train_dataloader)
         val_result = eval(model, device, val_dataloader)
         # TODO add tensorboard to visualize training over time
@@ -92,12 +104,20 @@ def model_test(dataset, model, config, train_dataset, val_dataset):
     #model = ST_GAT(in_channels=dataset.shape[1::], out_channels=dataset.shape[1::])
     # in testing, we should use the model that we've trained in order to evaluate the performance of the model
 
-    train_acc = eval(model, device, train_dataset)
-    valid_acc = eval(model, device, val_dataset)
-    test_acc = eval(model, device, dataset)
+    train_rmse, train_mae, train_mape = eval(model, device, train_dataset)
+    valid_rmse, valid_mae, valid_mape = eval(model, device, val_dataset)
+    test_rmse, test_mae, test_mape = eval(model, device, dataset)
     #train_acc, valid_acc, test_acc = eval(model, data, split_idx) 
 
     print(f'Test:, '
-          f'Train: {100 * train_acc:.2f}%, '
-          f'Valid: {100 * valid_acc:.2f}% '
-          f'Test: {100 * test_acc:.2f}%')
+          f'Train RMSE: {100 * train_rmse:.2f}, '
+          f'Valid RMSE: {100 * valid_rmse:.2f} '
+          f'Test RMSE: {100 * test_rmse:.2f}')
+    print(f'Test:, '
+          f'Train MAE: {100 * train_mae:.2f}, '
+          f'Valid MAE: {100 * valid_mae:.2f} '
+          f'Test MAE: {100 * test_mae:.2f}')
+    print(f'Test:, '
+          f'Train MAPE: {100 * train_mape:.2f}%, '
+          f'Valid MAPE: {100 * valid_mape:.2f}% '
+          f'Test MAPE: {100 * test_mape:.2f}%')
